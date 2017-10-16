2642c96332d44a1c559a717f1531b56af5dd6ed9
Coet <writerphone@163.com>
2017-10-16 11:56:35 +0800

    Revert "fix bugs in pmap.c"

--- kern/env.c
+++ kern/env.c
@@ -116,26 +116,18 @@ env_init(void)
 {
 	// Set up envs array
 	// LAB 1: Your code here.
-	//size_t sizeOfEnvs = sizeof(envs)/sizeof(*envs);
-	/*
-	for(size_t t = 0; t < NENV; t++){
+	size_t sizeOfEnvs = sizeof(envs)/sizeof(*envs);
+	for(size_t t = 0; t < sizeOfEnvs; t++){
 
 		envs[t].env_status = ENV_FREE;
 		envs[t].env_id = 0;
 
-		if(t != NENV - 1){
+		if(t != sizeOfEnvs - 1){
 			envs[t].env_link = &envs[t+1];
 		}
 	}
 	env_free_list = envs;
-	*/
-	env_free_list = NULL;
-	for(size_t i = NENV-1; i+1; --i){
-		envs[i].env_id = 0;
-		envs[i].env_status = ENV_FREE;
-		envs[i].env_link = env_free_list;
-		env_free_list = envs + i;	
-	}
+
 	// Per-CPU part of the initialization
 	env_init_percpu();
 }
@@ -392,13 +384,10 @@ void
 env_create(uint8_t *binary, enum EnvType type)
 {
 	// LAB 1: Your code here.
-	struct Env* new_env = NULL;
-	int32_t r = env_alloc(&new_env, 0);
-	if(r != 0){
-		panic("failed in env_create.");
-	}	
-	load_icode(new_env, binary);
-	new_env->env_type = type;
+	struct Env** new_env = NULL;
+	env_alloc(new_env, 0);
+	load_icode(new_env[0], binary);
+	new_env[0]->env_type = ENV_TYPE_USER;
 }
 
 //
@@ -520,9 +509,9 @@ env_run(struct Env *e)
 	}
 	curenv = e;
 	curenv->env_status = ENV_RUNNING;
-	++curenv->env_runs;
-	lcr3(PADDR(curenv->env_pgdir));
-	env_pop_tf(&curenv->env_tf);
+	curenv->env_runs++;
+	lcr3(*(curenv->env_pgdir));
+	env_pop_tf(&(curenv->env_tf));
 	
 	//panic("env_run not yet implemented");
 }
